34c4b92afc78f622e0c419a44203197499e163e5
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-11-16 10:13:47 +0300

    Update env_builtins.c

--- env_builtins.c
+++ env_builtins.c
@@ -3,7 +3,6 @@
 int shellby_env(char **args, char __attribute__((__unused__)) **front);
 int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
 int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
-
 /**
  * shellby_env - Prints the current environment.
  * @args: An array of arguments passed to the shell.
@@ -78,15 +77,12 @@ int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
 
 	return (0);
 }
-
 /**
  * shellby_unsetenv - Deletes an environmental variable from the PATH.
  * @args: An array of arguments passed to the shell.
  * @front: A double pointer to the beginning of args.
  * Description: args[1] is the PATH variable to remove.
- *
  * Return: If an error occurs - -1.
- *         Otherwise - 0.
  */
 int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front)
 {
